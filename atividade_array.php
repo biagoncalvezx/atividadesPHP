fa9095abd88b628062c648326354d7fd30ec96a2
bia <beatriz.g.campos@aluno.senai.br>
2025-08-14 15:43:13 -0300

    Atividade senai PHP

---
+++ atividade_array.php
@@ -0,0 +1,14 @@
+<?php
+    $frutas = [
+        "Maçã",
+        "Pera",
+        "Morango",
+        "Banana",
+        "Limão",
+        "Laranja",
+    ];
+    echo $frutas[0]."<br>"; 
+    for($i =1; $i <= 5; $i++) {
+        echo $frutas[$i]."<br>";
+    }
+?>
