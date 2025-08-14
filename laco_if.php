3d827c835dac26651fdb67c8f259eac4f16816a4
bia <beatriz.g.campos@aluno.senai.br>
2025-08-14 15:29:02 -0300

    Atividades senai PHP 14/08/2025

---
+++ laco_if.php
@@ -0,0 +1,20 @@
+<?php
+    $resposta = "sim";
+    $pergunta = "Sua mãe deixou você ir ao Mcdonald?";
+
+    if ($resposta == "sim") {
+        echo "O role ta on";
+    }else{
+        echo "Hoje não tem role";
+    }
+
+    echo "<br>";
+    $n1 = 100;
+    $n2 = 200;
+    if($n1 < $n2){
+        echo "$n1 é menor que $n2";
+
+    }else{
+        echo "$n1 é maior que $n2";
+    }
+?>
---
+++ operador_or.php
@@ -0,0 +1,12 @@
+<?php
+$feriado = false;
+$FimDeSemana = true;
+
+if ($feriado || $FimDeSemana) {
+    echo "Pode descansar.";
+} else {
+    echo "Dia útil de trabalho";
+
+}
+?>
+|
